041571456791e9094f4b1847339fc79a423516d5
Timo Bakx <timobakx@gmail.com>
2021-06-17 11:14:23 +0200

    Added default bindings for MailHog

---
+++ mailhog/config.zsh
@@ -0,0 +1,5 @@
+export MH_API_BIND_ADDR=mail.dev.timobakx.nl:1080
+export MH_UI_BIND_ADDR=mail.dev.timobakx.nl:1080
+export MH_SMTP_BIND_ADDR=127.0.0.1:1025
+export MH_HOSTNAME=mail.dev.timobakx.nl
+
